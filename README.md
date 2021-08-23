8d55f28a008c4e514fd45d88f426e8bcee6d3c36
Damien Cuvillier <damien@gotan.io>
2021-08-23 14:30:34 +0200

    v0.1 - first working version

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # screen.js
-Cross browser screen rotation management in javascript
+Cross browser screen rotation management in pure vanilla javascript
+
+## Installation 
+
+npm install screen-resolution.js
+
+## Usage
+
+Full example on 
+
+### Initialization
+
+```js
+let so = screenorientation();
+```
+
+### Change listener
+```js
+// Event listener 
+// On screen resolution changes
+// Returns screen resolution instance.
+so.change(callback);
+```
+
+You can trigger manually your event by calling with null argument. 
+```js
+// Trigger manually change orientation event
+so.change(); 
+```
+### Values
+```js
+// Returns rotation angle : 0 or 90
+so.angle()
+
+// Landscape display mode ? 
+so.isLandscape()
+
+// Portrait display mode  ?
+so.isPortrait()
+
+```
+
+
+
+
+### License
+
+MIT License
+
---
+++ screen-resolution.js
@@ -0,0 +1,100 @@
+/** Tools for screen orientation in web app
+ *
+ * @author Damien Cuvillier <damien.cuvillier@gotan.io>
+ * @licence MIT
+ *
+ */
+var screenorientation = (function() {
+    /** Listeners lists on orientation change event */
+    screenOrientationChangeListenerCallbacks = [];
+
+
+    /**  Detect screen orientation change.
+     * Calls all declared callbacks.
+     **/
+    function detectScreenOrientationChange() {
+        if (navigator.userAgent.indexOf('Safari') !== -1) {
+            window.matchMedia("(orientation: portrait)").addListener(triggerOrientationChange)
+        } else if (navigator.userAgent.indexOf('Mozilla') !== -1) {
+            window.screen.onmozorientationchange = triggerOrientationChange;
+        } else {
+            window.screen.orientation.onchange = triggerOrientationChange;
+        }
+    }
+
+    /** Trigger all declared callbacks
+     */
+    function triggerOrientationChange() {
+        for (let listener of screenOrientationChangeListenerCallbacks) {
+            listener(_self);
+        }
+    }
+
+    /** Get screen orientation angle.
+     * @returns {number} 90|0
+     */
+    function getAngle() {
+        if (window.matchMedia('(orientation: portrait)').matches) {
+            return 90;
+        }
+        if (window.matchMedia('(orientation: landscape)').matches) {
+            return 0;
+        }
+
+        if (window.orientation != null) {
+            // Safari
+            return parseInt(window.orientation, 0);
+        }
+        return 0;
+    }
+
+    /**
+     * Add listener or trigger change event if no callback provided.
+     *
+     * @param callback null|function Call back function on orientation change event
+     */
+    function onchange(callback) {
+        // Trigger event
+        if (typeof callback === 'undefined') {
+            triggerOrientationChange();
+            return this;
+        }
+        // Add listener
+        if (typeof callback !== 'function'){
+            throw 'Provided callback for screen orientation change is not a function';
+        }
+        screenOrientationChangeListenerCallbacks.push(callback);
+        return this;
+    }
+
+
+
+    /** Is screen in landscape mode.
+     */
+    function isLandscape() {
+        return Math.abs(getAngle() / 90) !== 1;
+    }
+
+    /** Is screen in portrait mode.
+     */
+    function isPortrait() {
+        return !isLandscape();
+    }
+
+    /* Constructor */
+    detectScreenOrientationChange();
+
+
+    /* Public features. */
+    const _self = {
+        'angle': getAngle,
+        'isLandscape': isLandscape,
+        'isPortrait': isPortrait,
+        'change': onchange
+    }
+
+    return _self;
+})
+
+
+
